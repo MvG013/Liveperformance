588a642ee8c6aae68043b4e866998886c3e08970
MvG013 <marcvangool@hotmail.com>
2016-06-22 16:35:45 +0200

    Begin klassen implementeren

---
+++ Liveperformance/Liveperformance/Klassen/Administrator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liveperformance
+{
+    class Administrator
+    {
+        string Gebruikersnaam { get; set; }
+
+    }
+}
